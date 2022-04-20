6a6d646eda5e529dcc34f7cabe392ced2a0d9f73
mkameronh <90940985+mkameronh@users.noreply.github.com>
2022-04-20 13:23:32 -0500

    Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,4 @@
+library(dplyr)
+mechacar_mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_mpg_table)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_mpg_table))
